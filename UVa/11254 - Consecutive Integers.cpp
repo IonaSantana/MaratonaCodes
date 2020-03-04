4dcc010c06ad111e369bc7ff3d0a0949249bd1f1
IonaSantana <47615109+IonaSantana@users.noreply.github.com>
2020-03-04 19:18:03 -0300

    Create 11254 - Consecutive Integers.cpp

---
+++ UVa/11254 - Consecutive Integers.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	long long n,s,resp,teste,teste1;
+	int tam;
+	while (scanf("%lld",&n), n!=-1)
+	{
+		vector<int>p;
+		s = sqrt(2*n);
+		for (int i = s; i >= 1; i--)
+		{
+			if((2*n)%i == 0)
+			{			
+				p.push_back(i);
+			}	
+		}
+		tam = p.size();
+		teste = teste1 = n;
+		for (int i = 0; i < tam-1; i++)
+		{
+			long long a = (2*n + p[i] - p[i]*p[i])/(2*p[i]);	
+			resp = ((2*a+p[i]-1)*p[i])/2;
+			if (resp == n)
+			{
+				teste = a;
+				teste1 = a+p[i]-1;
+				break;
+			}												
+		}
+		printf("%lld = %lld + ... + %lld\n",n,teste,teste1);
+	}
+}
